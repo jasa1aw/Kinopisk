23c4d1ae8de6b2ca11e26a39ff2c686f0436b266
jasa1aw <erakrut05@gmail.com>
2023-09-23 23:09:45 +0600

    registration block 'register.ejs' was created and added

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.get('/', (req, res) => {
 app.get('/login', (req, res) => {
     res.render('login')
 })
+app.get('/register', (req, res) => {
+    res.render('register')
+})
 
 const PORT = 8000;
 app.listen(PORT, () => {
