203cb7b7177f83fca722182305a49ac9fbec3311
jasa1aw <erakrut05@gmail.com>
2023-09-24 15:21:50 +0600

    moved all endpoints to router(router.js) and connected the database(MongoDB)

--- server.js
+++ server.js
@@ -2,30 +2,12 @@ const express =  require('express');
 
 const app = express()
 
+require('./server/config/db')
+
 app.use(express.static('public'));
 app.set("view engine", "ejs")
+app.use(require('./server/pages/router'))
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
-app.get('/login', (req, res) => {
-    res.render('login')
-})
-app.get('/register', (req, res) => {
-    res.render('register')
-})
-app.get('/profile', (req, res) => {
-    res.render('profile')
-})
-app.get('/admin', (req, res) => {
-    res.render('adminProfile')
-})
-app.get('/new', (req, res) => {
-    res.render('newFilm')
-})
-app.get('/edit', (req, res) => {
-    res.render('editFilm')
-})
 
 const PORT = 8000;
 app.listen(PORT, () => {
