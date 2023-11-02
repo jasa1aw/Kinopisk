42c0e451a4847720cf30d175308edc3c4b8fd4a9
jasa1aw <erakrut05@gmail.com>
2023-11-02 20:02:32 +0600

    implemented comment logic and movie rating

---
+++ server/Rates/controller.js
@@ -0,0 +1,15 @@
+const Rate = require('./Rates');
+
+const saveRate = async(req, res) =>{
+    if(req.body.authorId && req.body.filmId && req.body.rate)
+    await new Rate({
+        rate: req.body.rate,
+        text: req.body.text,
+        authorId: req.body.authorId,
+        filmId: req.body.filmId,
+    }).save()
+    // console.log(req.body);
+    res.status(200).send(true);
+}
+
+module.exports = {saveRate}
