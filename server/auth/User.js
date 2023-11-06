91bc37c423cf8dd92aed3023f05cb5c1af0f16a8
jasa1aw <erakrut05@gmail.com>
2023-11-06 22:48:40 +0600

    edit mistake on userSchema

--- server/auth/User.js
+++ server/auth/User.js
@@ -8,7 +8,7 @@ const UserSchema = new mongoose.Schema({
     isAdmin: Boolean,
     googleId: String,
     toWatch: [{type: Schema.Types.ObjectId, ref: 'film'}],
-    wathed: [{type: Schema.Types.ObjectId, ref: 'film'}]
+    watched: [{type: Schema.Types.ObjectId, ref: 'film'}]
 })
 
 module.exports = mongoose.model('user', UserSchema)
